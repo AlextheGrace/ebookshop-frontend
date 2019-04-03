30ad9751301c0f029d0e722c2a40a5da072500cb
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-03 18:34:18 +0200

    add back styles and change theme

--- src/index.js
+++ src/index.js
@@ -4,30 +4,41 @@ import './index.css';
 import { Book } from './components';
 import Main from './Main';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blueGrey from "@material-ui/core/colors/blueGrey";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const theme = createMuiTheme(
-    {
-        palette: {
-          theme: 'dark'
-        },
-      }
-);
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: lightGreen[300],
+			main: lightGreen[500],
+			dark: lightGreen[700]
+		},
+		secondary: {
+			light: blueGrey[300],
+			main: blueGrey[500],
+			dark: blueGrey[700]
+		},
+		type: 'dark'
+	}
+});
 
 console.log(theme);
 
-
-render(<MuiThemeProvider theme={theme}>
-			<Router>
-        <Switch>
-          <Route path="/" component={Main} />
-          <Route path='/books' render={(props) => <Book {...props} isAuthed={true} />} />
-        </Switch>
-			</Router>             
-      </MuiThemeProvider>,
-
-     document.getElementById('root'));
+render(
+	<MuiThemeProvider theme={theme}>
+		<Router>
+			<Switch>
+				<Route path="/" component={Main} />
+				<Route path="/books" render={props => <Book {...props} isAuthed={true} />} />
+			</Switch>
+		</Router>
+	</MuiThemeProvider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
