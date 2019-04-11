e48877a3cd2c04c8718367aa8b0c4db760f969af
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-11 16:08:05 +0200

    add paypal express checkout

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,14 +5,15 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import StripeCheckout from 'react-stripe-checkout';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
 import './books.css';
 
 const styles = {
 	root: {},
 	GridContainer: {
-    maxWidth: 50,
-    display: 'flex',
-    justifyContent: 'center'
+		maxWidth: 50,
+		display: 'flex',
+		justifyContent: 'center'
 	},
 	Grid: {
 		display: 'flex',
@@ -41,26 +42,24 @@ class Book extends Component {
 		this.state = {
 			book: {}
 		};
-  }
-  
-  onToken = (token) => {
-    const body = {
-      amount: 666,
-      token: token
-    }
-    axios
-    .post("http://localhost:5000/api/checkout", body)
-    .then(res => {
-      console.log(res);
-      alert("Payment Success");
-    })
-    .catch(error => {
-      console.log("Payment Error: ", error);
-      alert("Payment Error");
-    });
-};
-  
+	}
 
+	onToken = token => {
+		const body = {
+			amount: 666,
+			token: token
+		};
+		axios
+			.post('http://localhost:5000/api/checkout', body)
+			.then(res => {
+				console.log(res);
+				alert('Payment Success');
+			})
+			.catch(error => {
+				console.log('Payment Error: ', error);
+				alert('Payment Error');
+			});
+	};
 
 	componentDidMount() {
 		axios.get(`${process.env.REACT_APP_API_URL}/books/${this.props.match.params.title}`).then(res => {
@@ -72,6 +71,35 @@ class Book extends Component {
 	render() {
 		const { classes } = this.props;
 		const { book } = this.state;
+		const onSuccess = payment => {
+			// Congratulation, it came here means everything's fine!
+			console.log('The payment was succeeded!', payment);
+			// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+		};
+
+		const onCancel = data => {
+			// User pressed "cancel" or close Paypal's popup!
+			console.log('The payment was cancelled!', data);
+			// You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+		};
+
+		const onError = err => {
+			// The main Paypal's script cannot be loaded or somethings block the loading of that script!
+			console.log('Error!', err);
+			// Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+			// => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+		};
+
+		let env = 'sandbox'; // you can set here to 'production' for production
+		let currency = 'USD'; // or you can set this value from your props or state
+		let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+		// Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
+
+		const client = {
+			sandbox: 'YOUR-SANDBOX-APP-ID',
+			production: 'YOUR-PRODUCTION-APP-ID'
+		};
+
 		return (
 			<div className="Book">
 				<Header />
@@ -86,18 +114,26 @@ class Book extends Component {
 							Buy
 						</Button>
 
-
 						<StripeCheckout
 							amount={book.price}
 							billingAddress
 							description={book.title}
-              image="https://res.cloudinary.com/ikarus-books/image/upload/v1554314839/ikaruslogocropped2.png"
-              token={this.onToken}
+							image="https://res.cloudinary.com/ikarus-books/image/upload/v1554314839/ikaruslogocropped2.png"
+							token={this.onToken}
 							locale="auto"
 							name="Ikarus Books"
 							stripeKey="pk_test_1ZH6zDjZwu9QY27YC61eaA1Y00SflpwI85"
-              zipCode
-              email
+							zipCode
+							email
+						/>
+						<PaypalExpressBtn
+							env={env}
+							client={client}
+							currency={currency}
+							total={total}
+							onError={onError}
+							onSuccess={onSuccess}
+							onCancel={onCancel}
 						/>
 					</Grid>
 				</Grid>
