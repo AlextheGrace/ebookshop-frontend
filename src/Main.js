c10c83d6f573bfe4c9a80ad8f7d4d123901311bc
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-12 15:31:39 +0200

    add footer to main

--- src/Main.js
+++ src/Main.js
@@ -1,42 +1,43 @@
 import React, { Component } from 'react';
-import { Header } from './components/layout';
+import { Header, Footer } from './components/layout';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Switch, Route } from 'react-router-dom';
-import { Books, Authors, Contact, Book} from './components'
+import { Books, Authors, Contact, Book } from './components';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		padding: theme.spacing.unit * 2,
+		textAlign: 'center',
+		color: theme.palette.text.secondary
+	}
 });
 
 class Main extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className="Main">
-      <Header/>
-        <Switch>
-          <Route exact path="/" component={Books}/>
-          <Route path="/authors" component={Authors}/>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/books/:title" component={Book}/>
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className="Main">
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Books} />
+					<Route path="/authors" component={Authors} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/books/:title" component={Book} />
+				</Switch>
+        <Footer />
+			</div>
+		);
+	}
 }
 
 Main.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Main);
