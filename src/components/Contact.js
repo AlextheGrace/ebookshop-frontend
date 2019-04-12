1a597a99a6d5b8e0816a1d204fd4f86f6606b0d4
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-12 15:18:09 +0200

    remove header Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { Header } from './layout'
+import { Header } from './layout';
 import Grid from '@material-ui/core/Grid';
 
 class Contact extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      
-      <div className="Contact">
-      <Header></Header>
-      <Grid container spacing={24} style={{padding: 40}}>
-        Contact
-      </Grid>
-      </div>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className="Contact">
+				<Grid container spacing={24} style={{ padding: 40 }}>
+					Contact
+				</Grid>
+			</div>
+		);
+	}
 }
 
 export default Contact;
