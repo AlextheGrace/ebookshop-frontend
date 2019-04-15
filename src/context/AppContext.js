f6ae51a51a2d8a77c44fe398d885bab120f30df0
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-16 00:11:34 +0200

    work in context component

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (AppContext = React.createContext()); 
+const AppContext = React.createContext();
+
+export const AppConsumer = AppContext.Consumer;
+
+class AppProvider extends Component {
+	state = {
+		username: 'alexx',
+		password: null,
+		isAuth: false,
+		token: null
+	};
+	render() {
+		return (
+			// value prop is where we define what values
+			// that are accessible to consumer components
+			<AppContext.Provider value={this.state}>{this.props.children}</AppContext.Provider>
+		);
+	};
+};
+
+export default AppProvider;
