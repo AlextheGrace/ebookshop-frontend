c91d2fa7e60279926c1f31a6741e40bfb46ea679
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-26 14:41:01 +0200

    implemented nice effect wish there were more books

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
 import axios from 'axios';
 import './books.css';
 
@@ -26,9 +28,9 @@ const styles = {
 	bookItem: {
 		maxWidth: 200,
 		maxHeight: 300,
-		shadowOffset:{  width: 10,  height: 10,  },
+		shadowOffset: { width: 10, height: 10 },
 		shadowColor: 'black',
-		shadowOpacity: 1.0,
+		shadowOpacity: 1.0
 	}
 };
 
@@ -46,8 +48,8 @@ class Books extends Component {
 	}
 
 	componentDidMount() {
-			
-		axios.get(process.env.REACT_APP_API_URL+'/books')
+		axios
+			.get(process.env.REACT_APP_API_URL + '/books')
 			.then(res => {
 				this.setState({ books: res.data });
 			})
@@ -55,7 +57,8 @@ class Books extends Component {
 				console.log(`error getting books: ${error}`);
 			});
 
-		axios.get(process.env.REACT_APP_API_URL+'/books/latest')
+		axios
+			.get(process.env.REACT_APP_API_URL + '/books/latest')
 			.then(res => {
 				this.setState({ featuredBooks: res.data });
 			})
@@ -70,27 +73,24 @@ class Books extends Component {
 
 		return (
 			<div className={classes.root}>
-
-				<Grid container  spacing={16} style={{ padding: 40 }}>
+				<Grid container spacing={16} style={{ padding: 40 }}>
 					<Grid item xs={12} className={classes.Grid}>
 						<img
 							src="https://res.cloudinary.com/ikarus-books/image/upload/v1554314839/ikaruslogocropped2.png"
 							alt="ikarus logo"
 						/>
 					</Grid>
-					<Grid item xs={12}  className={classes.Grid}>
+					<Grid item xs={12} className={classes.Grid}>
 						<h1>Latest publications</h1>
 					</Grid>
 
 					{featuredBooks.map(book => (
-							<Grid item xs={12} sm={6} md={4} className={classes.Grid}>
-							
-								<Link to={`/books/${book._id}`}>
-									<img className="books" src={book.img} alt={book.title} />
-									
-								</Link>
-							</Grid>
-						))}
+						<Grid item xs={12} sm={6} md={4} className={classes.Grid}>
+							<Link to={`/books/${book._id}`}>
+								<img className="books" src={book.img} alt={book.title} />
+							</Link>
+						</Grid>
+					))}
 
 					<Grid container spacing={16} style={{ padding: 20 }}>
 						<Grid item justify="flex-start" xs={12} className={classes.Grid}>
@@ -98,10 +98,8 @@ class Books extends Component {
 						</Grid>
 						{books.map(book => (
 							<Grid item xs={12} sm={6} md={4} className={classes.Grid}>
-							
 								<Link to={`/books/${book._id}`}>
-									<img className="books" src={book.img} alt={book.title} />
-									
+									<LazyLoadImage className="books" src={book.img} alt={book.title} effect="opacity" />
 								</Link>
 							</Grid>
 						))}
