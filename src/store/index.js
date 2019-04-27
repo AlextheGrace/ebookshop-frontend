23642734584b3a150fb25596c8e1c32c21278977
Alexander Grace <alexander.grace@chasacademy.se>
2019-04-27 16:37:37 +0200

    remove devtools

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
 import thunk from 'redux-thunk'
 import { applyMiddleware,  compose,  createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
-import devTools from 'remote-redux-devtools';
+// import devTools from 'remote-redux-devtools';
 
 const middleware = [thunk];
 
