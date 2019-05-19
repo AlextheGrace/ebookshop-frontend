191252761de88a6611d40160284b128db4bf5635
Alexander Grace <alexander.grace@chasacademy.se>
2019-05-19 13:55:57 +0200

    refactor auth login request

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,92 +7,94 @@ const LOGOUTFAILURE = 'LOGOUT_FAILURE';
 const LOGOUTSUCCESS = 'LOGOUT_SUCCESS';
 
 const initialState = {
-  error: null,
-  isFetching: false,
-  user: {},
-  auth: false
-}
+	error: null,
+	isFetching: false,
+	user: {},
+	auth: false
+};
 export default (reduxState = initialState, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case LOGINREQUEST:
-      return {
-        ...reduxState,
-        isFetching: true,
-        
-      }
-    case LOGINSUCCESS:
-      return {
-        ...reduxState,
-        isFetching: false,
-        auth: payload.auth,
-        user: payload.user
-      }
-    case LOGINFAILURE:
-      return {
-        ...reduxState,
-        error: payload.error,
-        isFetching: false,
-      }
-    case LOGOUTSTART:
-      return {
-        ...reduxState,
-        isFetching: false,
-      }
-    case LOGOUTFAILURE:
-      return {
-        ...reduxState,
-        isFetching: false,
-      }
-    case LOGOUTSUCCESS:
-      return {
-        ...reduxState,
-        isFetching: false,
-        user: {},
-        auth: false
-      }
-    default:
-      return reduxState
-  }
-}
+	const { type, payload } = action;
+	switch (type) {
+		case LOGINREQUEST:
+			return {
+				...reduxState,
+				isFetching: true
+			};
+		case LOGINSUCCESS:
+			return {
+				...reduxState,
+				isFetching: false,
+				auth: payload.auth,
+				user: payload.user
+			};
+		case LOGINFAILURE:
+			return {
+				...reduxState,
+				error: payload.error,
+				isFetching: false
+			};
+		case LOGOUTSTART:
+			return {
+				...reduxState,
+				isFetching: false
+			};
+		case LOGOUTFAILURE:
+			return {
+				...reduxState,
+				isFetching: false
+			};
+		case LOGOUTSUCCESS:
+			return {
+				...reduxState,
+				isFetching: false,
+				user: {},
+				auth: false
+			};
+		default:
+			return reduxState;
+	}
+};
 export const loginAdmin = (username, password) => dispatch => {
-  dispatch({ type: LOGINREQUEST })
-  return axios.post(`${process.env.REACT_APP_API_URL}/auth/login`,{ username, password})
-    .then((response) => {
-      console.log(response);
-      localStorage.setItem('user', response.data.token);
-      dispatch({
-        type: LOGINSUCCESS,
-        payload: {
-          user: response.data.user,
-          auth: response.data.auth,
-
-        },
-      })
-    })
-    .catch((error) => {
-      dispatch({
-        type: LOGINFAILURE,
-        payload: {
-          error,
-          auth: false
-        },
-      })
-    })
-}
+	dispatch({ type: LOGINREQUEST });
+	return axios({
+    method: 'post',
+		url: `${process.env.REACT_APP_API_URL}/auth/login`,
+		data: { username, password },
+		withCredentials: false
+	})
+		.then(response => {
+			console.log(response);
+			localStorage.setItem('user', response.data.token);
+			dispatch({
+				type: LOGINSUCCESS,
+				payload: {
+					user: response.data.user,
+					auth: response.data.auth
+				}
+			});
+		})
+		.catch(error => {
+			dispatch({
+				type: LOGINFAILURE,
+				payload: {
+					error,
+					auth: false
+				}
+			});
+		});
+};
 
 export const logoutAdmin = () => dispatch => {
-  dispatch({ type: LOGOUTSTART });
+	dispatch({ type: LOGOUTSTART });
 
-  try {
-    localStorage.removeItem('user');
-    return dispatch({
-        type: LOGOUTSUCCESS
-    })
-} catch (e) {
-    return dispatch({
-        type: LOGOUTFAILURE
-    })
-
-}
-}
+	try {
+		localStorage.removeItem('user');
+		return dispatch({
+			type: LOGOUTSUCCESS
+		});
+	} catch (e) {
+		return dispatch({
+			type: LOGOUTFAILURE
+		});
+	}
+};
